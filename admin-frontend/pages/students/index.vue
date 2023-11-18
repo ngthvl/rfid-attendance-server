83b0c2404d8aa42aa77b6227d2247a34b2f7055b
Teejay Tamani <ttamani@multisyscorp.com>
2023-11-19 04:17:05 +0800

    -

---
+++ admin-frontend/pages/students/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+import jwtMiddleware from "../../middleware/jwtMiddleware";
+
+import {list as listAdmins, admins} from '~/models/admin'
+import {SymbolKind} from "vscode-languageserver-types";
+import Array = SymbolKind.Array;
+
+listAdmins();
+
+definePageMeta({
+  middleware: jwtMiddleware,
+  layout: 'admin',
+})
+
+</script>
+
+<template>
+  <v-container>
+    <v-card class="shadown mt-4 mx-5">
+      <v-card-item>
+        <v-text-field class="mt-3" prepend-inner-icon="mdi-magnify" variant="outlined" label="Search"></v-text-field>
+      </v-card-item>
+      <v-card-item>
+        <v-table :hover="true">
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>-</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(admin, key) in admins">
+            <td>{{ admin.name }}</td>
+            <td>{{ admin.email }}</td>
+            <td></td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-card-item>
+      <v-card-item>
+        <v-pagination :length="10" total-visible="10"></v-pagination>
+      </v-card-item>
+    </v-card>
+  </v-container>
+</template>
---
+++ api/modules/sms-service/src/Http/Resources/PhoneBookResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tamani\Admin\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Tamani\Admin\Models\Admin;
+
+class AdminResource extends JsonResource
+{
+    /*
+     * @var Tamani\Admin\Models\Admin
+     */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return $this->resource->only(array_merge(Admin::FILLABLE, [
+            'id', 'created_at'
+        ]));
+    }
+}
