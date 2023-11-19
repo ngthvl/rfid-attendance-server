afefa1d2d96e4cff7ed46319e4f4efa7027e901b
Teejay Tamani <ttamani@multisyscorp.com>
2023-11-19 14:17:28 +0800

    -

--- admin-frontend/layouts/admin.vue
+++ admin-frontend/layouts/admin.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 
 import SideBar from "~/components/SideBar.vue";
+import {isApiLoading} from "~/composables/useApi";
 </script>
 
 <template>
   <v-layout>
     <v-main>
       <SideBar />
+      <v-progress-linear
+          v-if="isApiLoading"
+          indeterminate
+          color="cyan"
+      ></v-progress-linear>
       <MenuBar />
       <NuxtPage />
     </v-main>
