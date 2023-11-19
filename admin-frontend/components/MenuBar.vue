089cff849700d5334ea4a27e68c5719d0e862bc8
Teejay Tamani <ttamani@multisyscorp.com>
2023-11-19 18:05:32 +0800

    -

--- admin-frontend/components/MenuBar.vue
+++ admin-frontend/components/MenuBar.vue
@@ -5,19 +5,30 @@ import {isApiLoading} from "~/composables/useApi";
 
 <template>
   <v-app-bar :elevation="2">
-    <v-app-bar-title>Admininstration</v-app-bar-title>
-    <v-app-bar-nav-icon>
-      <v-icon icon="mdi-account-circle-outline"></v-icon>
-      <v-menu activator="parent">
-        <v-list>
-          <v-list-item title="Account Settings" to="/profile"></v-list-item>
-          <v-list-item title="Logout" @click="useLogout"></v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar-nav-icon>
+    <template #default>
+      <v-app-bar-title>Admininstration</v-app-bar-title>
+      <v-app-bar-nav-icon>
+        <v-icon icon="mdi-account-circle-outline"></v-icon>
+        <v-menu activator="parent">
+          <v-list>
+            <v-list-item title="Account Settings" to="/profile"></v-list-item>
+            <v-list-item title="Logout" @click="useLogout"></v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar-nav-icon>
+    </template>
+    <template #extension>
+      <v-progress-linear
+          v-if="isApiLoading"
+          indeterminate
+          color="cyan"
+      ></v-progress-linear>
+    </template>
   </v-app-bar>
 </template>
 
 <style scoped>
-
+.v-toolbar__extension{
+  height: 10px!important;
+}
 </style>
