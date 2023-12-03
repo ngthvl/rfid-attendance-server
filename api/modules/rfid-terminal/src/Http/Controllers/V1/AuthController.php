cfcb29df35ff569ee4ede852e8ab65978b209709
Teejay Tamani <ttamani@multisyscorp.com>
2023-12-04 05:46:49 +0800

    -

--- api/modules/rfid-terminal/src/Http/Controllers/V1/AuthController.php
+++ api/modules/rfid-terminal/src/Http/Controllers/V1/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
 
         $user = RfidTerminal::where('ip_address', $deviceIp)->first();
 
-        if($user){
+        if($user && $user->id != $deviceId){
             $user->delete();
         }
 
