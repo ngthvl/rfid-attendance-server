c67fd6fad3255c9738f11e9ac72a236bc0bd87d0
Teejay Tamani <teejay.tamani@appetiser.com.au>
2023-12-11 06:14:15 +0800

    -

--- sms-server/server.py
+++ sms-server/server.py
@@ -19,7 +19,7 @@ class smsServer:
     SERIAL_PARITY_BITS = serial.PARITY_NONE
     SERIAL_STOP_BITS = serial.STOPBITS_ONE
     SERIAL_BYTESIZE = serial.EIGHTBITS
-    SERIAL_TIMEOUT = 0.2
+    SERIAL_TIMEOUT = 0.1
 
     CURRENT_REPLY = None
 
@@ -62,15 +62,15 @@ class smsServer:
                         myvars[name.strip()] = var.rstrip()
                 print("file - " + repr(path))
                 data = self.send_sms(params=myvars)
-                if data == 'OK':
-                    webhook_url = myvars['WEBHOOK']
-                    t = threading.Thread(target=self.send_to_webhook(url=webhook_url, data=data))
-                    t.start()
-                    os.remove(path)
-                else:
-                    print("ERROR: {}", format(str(os.path.basename(path))))
+                # if data == 'OK':
+                #     webhook_url = myvars['WEBHOOK']
+                #     t = threading.Thread(target=self.send_to_webhook(url=webhook_url, data=data))
+                #     t.start()
+                os.remove(path)
+                # else:
+                #     print("ERROR: {}", format(str(os.path.basename(path))))
 
-                # time.sleep(0.5)
+                time.sleep(4)
 
     def send_to_webhook(self, url, data):
         try:
@@ -83,15 +83,71 @@ class smsServer:
     def send_sms(self, params):
         command = 'AT+SMSEND="{}",3,"{}"'.format(params['PHONE_NUMBER'], params['MESSAGE'])
         command = bytes(command, 'ascii')
+        response = self.send_at_command(command)
+        # result = re.search('\r\n\r\n(.*)\r\n', response.decode('ascii'))
+        print(response)
+        # if result is not None:
+        #     return str(result.group(1))
+        #
+        # return 'ERROR'
+
+    def send_at_command(self, command):
         command += b'\x0d\x0a'
         self.SERIAL_BUS.write(command)
-        response = self.SERIAL_BUS.readall()
-        result = re.search('\r\n\r\n(.*)\r\n', response.decode('ascii'))
-        print(response)
-        if result is not None:
-            return str(result.group(1))
+        return self.SERIAL_BUS.readall()
 
-        return 'ERROR'
+    def initialize_modem(self):
+        # print(self.send_at_command(b'+++'))
+        print(self.send_at_command(b'a'))
+        print(self.send_at_command(b'AT+VER?'))
+        print(self.send_at_command(b'AT+E?'))
+        print(self.send_at_command(b'AT+WKMOD?'))
+        print(self.send_at_command(b'AT+CALEN?'))
+        print(self.send_at_command(b'AT+NATEN?'))
+        print(self.send_at_command(b'AT+UATEN?'))
+        print(self.send_at_command(b'AT+CMDPW?'))
+        print(self.send_at_command(b'AT+CACHEN?'))
+        print(self.send_at_command(b'AT+STMSG?'))
+        print(self.send_at_command(b'AT+ICCID?'))
+        print(self.send_at_command(b'AT+IMEI?'))
+        print(self.send_at_command(b'AT+CNUM?'))
+        print(self.send_at_command(b'AT+UART?'))
+        print(self.send_at_command(b'AT+RFCEN?'))
+        print(self.send_at_command(b'AT+APN?'))
+        print(self.send_at_command(b'AT+SOCKA?'))
+        print(self.send_at_command(b'AT+SOCKB?'))
+        print(self.send_at_command(b'AT+SOCKAEN?'))
+        print(self.send_at_command(b'AT+SOCKASL?'))
+        print(self.send_at_command(b'AT+SOCKALK?'))
+        print(self.send_at_command(b'AT+SOCKC?'))
+        print(self.send_at_command(b'AT+SOCKD?'))
+        print(self.send_at_command(b'AT+SOCKCEN?'))
+        print(self.send_at_command(b'AT+SOCKDEN?'))
+        print(self.send_at_command(b'AT+SOCKCSL?'))
+        print(self.send_at_command(b'AT+SOCKDSL?'))
+        print(self.send_at_command(b'AT+SOCKCLK?'))
+        print(self.send_at_command(b'AT+SOCKDLK?'))
+        print(self.send_at_command(b'AT+SOCKRSTIM?'))
+        print(self.send_at_command(b'AT+REGEN?'))
+        print(self.send_at_command(b'AT+REGTP?'))
+        print(self.send_at_command(b'AT+REGID?'))
+        print(self.send_at_command(b'AT+REGDT?'))
+        print(self.send_at_command(b'AT+REGSND?'))
+        print(self.send_at_command(b'AT+CLOUDEN?'))
+        print(self.send_at_command(b'AT+CLOUDID?'))
+        print(self.send_at_command(b'AT+HEARTEN?'))
+        print(self.send_at_command(b'AT+HEARTDT?'))
+        print(self.send_at_command(b'AT+HEARTTP?'))
+        print(self.send_at_command(b'AT+HEARTTM?'))
+        print(self.send_at_command(b'AT+HTPTP?'))
+        print(self.send_at_command(b'AT+HTPURL?'))
+        print(self.send_at_command(b'AT+HTPSV?'))
+        print(self.send_at_command(b'AT+HTPHD?'))
+        print(self.send_at_command(b'AT+HTPTIM?'))
+        print(self.send_at_command(b'AT+DSTNUM?'))
+
+        print(self.send_at_command(b'AT+WKMOD="SMS"'))
+        pass
 
     def initialize_serial_bus(self):
         print(self.SERIAL_PORT)
@@ -113,6 +169,10 @@ class smsServer:
             self.MODEM_RESPONSIVE = True
 
             print("Connected to serial device")
+
+            time.sleep(3)
+
+            self.initialize_modem()
         except:
             print("Serial Port Unavailable Retrying...")
             time.sleep(2)
