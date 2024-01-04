75565b1aca15b7b27397b5b6e113dc96579b8e73
Teejay Tamani <teejay.tamani@appetiser.com.au>
2024-01-04 23:58:16 +0800

    -

--- api/modules/rfid-terminal/src/Http/Controllers/V1/AttendanceController.php
+++ api/modules/rfid-terminal/src/Http/Controllers/V1/AttendanceController.php
@@ -21,9 +21,9 @@ class AttendanceController extends Controller
         $uid = $request->input('id');
         $ts = $request->input('ts', time());
 
-        $baseLine = (Carbon::now())->sub('seconds', env('ATTENDANCE_INTERVAL_SECS', 60));
+//        $baseLine = (Carbon::now())->sub('seconds', env('ATTENDANCE_INTERVAL_SECS', 60));
 
-        $outlast = RfidOutput::where('detected_uid', $uid)->where('detection_dt', '<=', $baseLine)->first();
+//        $outlast = RfidOutput::where('detected_uid', $uid)->where('detection_dt', '<=', $baseLine)->first();
 
 //        if($outlast){
 //            return $this->respondWithError('ALREADY_DETECTED', 422, 'Throttled');
@@ -51,6 +51,6 @@ class AttendanceController extends Controller
             return $allocation->allocation;
         }
 
-        return $this->respondWithError('INVALID_ENTRY', 422, 'Unknown Tag');
+        return $this->respondWithError('INVALID_ENTRY', 500, 'Unknown Tag');
     }
 }
