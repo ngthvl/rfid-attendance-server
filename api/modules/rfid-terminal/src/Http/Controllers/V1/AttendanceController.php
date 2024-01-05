ef4d02889a9f97bf71b750cc5dbb81cda7479c6d
Teejay Tamani <teejay.tamani@appetiser.com.au>
2024-01-05 09:25:13 +0800

    -

--- api/modules/rfid-terminal/src/Http/Controllers/V1/AttendanceController.php
+++ api/modules/rfid-terminal/src/Http/Controllers/V1/AttendanceController.php
@@ -21,13 +21,13 @@ class AttendanceController extends Controller
         $uid = $request->input('id');
         $ts = $request->input('ts', time());
 
-        $baseLine = (Carbon::now())->sub('seconds', env('ATTENDANCE_INTERVAL_SECS', 60));
-
-        $outlast = RfidOutput::where('detected_uid', $uid)->where('detection_dt', '<=', $baseLine)->first();
-
-        if($outlast){
-            return $this->respondWithError('ALREADY_DETECTED', 422, 'Throttled');
-        }
+//        $baseLine = (Carbon::now())->sub('seconds', env('ATTENDANCE_INTERVAL_SECS', 60));
+//
+//        $outlast = RfidOutput::where('detected_uid', $uid)->where('detection_dt', '<=', $baseLine)->first();
+//
+//        if($outlast){
+//            return $this->respondWithError('ALREADY_DETECTED', 422, 'Throttled');
+//        }
 
         $allocation = RfidTagAllocation::where('tag_data', $uid)->first();
 
